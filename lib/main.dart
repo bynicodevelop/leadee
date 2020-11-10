283eb9944deff95c4be3232cf271050f137346b3
nico-develop <contact@nico-develop.fr>
2020-11-10 08:44:13 +0100

    Intégration de la camera

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:camera/camera.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:leadeetuto/screens/Camera.dart';
 import 'package:leadeetuto/screens/Guest.dart';
 import 'package:leadeetuto/screens/dashboard/Home.dart';
 import 'package:leadeetuto/screens/services/UserService.dart';
@@ -7,39 +9,50 @@ import 'package:leadeetuto/screens/services/UserService.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  final List<CameraDescription> cameras = await availableCameras();
+
   await Firebase.initializeApp();
 
-  runApp(App());
+  runApp(App(
+    cameras: cameras,
+  ));
 }
 
 class App extends StatelessWidget {
   UserService _userService = UserService();
+  final List<CameraDescription> cameras;
+
+  App({this.cameras});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Leadee',
-      home: StreamBuilder(
-        stream: _userService.user,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.active) {
-            if (snapshot.hasData) {
-              return HomeScreen();
-            }
-
-            return GuestScreen();
-          }
-
-          return SafeArea(
-            child: Scaffold(
-              body: Center(
-                child: Text('Loading...'),
-              ),
-            ),
-          );
-        },
+      home: CameraScreen(
+        cameras: cameras,
       ),
+
+      // StreamBuilder(
+      //   stream: _userService.user,
+      //   builder: (context, snapshot) {
+      //     if (snapshot.connectionState == ConnectionState.active) {
+      //       if (snapshot.hasData) {
+      //         return HomeScreen();
+      //       }
+
+      //       return GuestScreen();
+      //     }
+
+      //     return SafeArea(
+      //       child: Scaffold(
+      //         body: Center(
+      //           child: Text('Loading...'),
+      //         ),
+      //       ),
+      //     );
+      //   },
+      // ),
     );
   }
 }
